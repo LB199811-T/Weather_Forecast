8075b710070a085ee55569e92d9f0ba54ec5de45
LB199811-T <LB199811T@163.com>
2018-01-05 01:17:56 +0800

    布局文件

---
+++ app/src/main/java/com/lb199811_t/weather/weather_forecast/ChooseAreaFragment.java
@@ -0,0 +1,21 @@
+package com.lb199811_t.weather.weather_forecast;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2018/1/5.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
